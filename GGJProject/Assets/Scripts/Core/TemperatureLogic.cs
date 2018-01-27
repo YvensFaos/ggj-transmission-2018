32aaa721d58830a6c89b937a675bc5c43f04f946
Yvens Rebouças Serpa <yvensre@gmail.com>
2018-01-27 10:08:47 -0300

    Add Toolsbar and respective scripts for PowerBar and WeatherControl

---
+++ GGJProject/Assets/Scripts/Core/TemperatureLogic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TemperatureLogic : MonoBehaviour {
+
+    public Text TemperatureText;
+
+    void Start () {
+        if (TemperatureText == null)
+        {
+            TemperatureText = GetComponent<Text>();
+        }
+    }
+
+    public void ControlWeather(float newTemperature)
+    {
+        TemperatureText.text = newTemperature.ToString() + "ºC";
+
+        //TODO logic that controls the weathery effects
+    }
+}
