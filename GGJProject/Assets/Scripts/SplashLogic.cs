617c1a4f9c88b65dbf5159697cb789f7207d6ef8
YvensArqueiro <yvens@ar.quei.ro>
2018-01-26 21:08:44 -0300

    Add basic screens (Splash, Loading and MainMenu)

--- GGJProject/Assets/Scripts/SplashLogic.cs
+++ GGJProject/Assets/Scripts/SplashLogic.cs
@@ -4,11 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class SplashLogic : MonoBehaviour {
 
+    private void Start()
+    {
+        CallLoadingScreen();
+    }
+
     /// <summary>
     /// Esse método é chamado pelo evento ao final da animação da SplashScreen
     /// </summary>
     public void CallLoadingScreen()
     {
-        SceneManager.LoadScene("LoadingScene");
+        SceneManager.LoadScene("LoadingScreen");
     }
 }
