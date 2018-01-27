8e3abb1de6ca217e1c16aabccecff7bf52da52b7
Yvens Rebouças Serpa <yvensre@gmail.com>
2018-01-27 10:24:34 -0300

    Add basic CallButton logic, with enums and change color call

--- GGJProject/Assets/Scripts/Core/CallButtonLogic.cs
+++ GGJProject/Assets/Scripts/Core/CallButtonLogic.cs
@@ -1,16 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
+public enum CallState
+{
+    CALLING, ANSWERING, WAITING
+}
+
+[System.Serializable]
+[RequireComponent(typeof(Image))]
 public class CallButtonLogic : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public CallState CurrentCallState;
+
+    public Color AnsweringColor;
+    public Color CallingColor;
+    public Color WaitingColor;
+
+    public Image CallImage;
+
+    void Start () {
+        if (CallImage == null)
+        {
+            CallImage = GetComponent<Image>();
+        }
+
+        ChangeCallState(CallState.WAITING);
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ChangeCallState(CallState newCallState)
+    {
+        CurrentCallState = newCallState;
+
+        switch(CurrentCallState)
+        {
+            case CallState.ANSWERING:
+                CallImage.CrossFadeColor(AnsweringColor, 1.0f, false, true);
+                break;
+            case CallState.CALLING:
+                CallImage.CrossFadeColor(CallingColor, 1.0f, false, true);
+                break;
+            case CallState.WAITING:
+                CallImage.CrossFadeColor(WaitingColor, 1.0f, false, true);
+                break;
+        }
+    }
+
+    public void ClickCallButton()
+    {
+        switch (CurrentCallState)
+        {
+            case CallState.CALLING:
+                break;
+            default:
+                //TODO play a buzz sound or something
+                break;
+        }
+    }
 }
