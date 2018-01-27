f0a05b5b4ab8a1701c80b68f74b06729fed28132
Yvens Rebouças Serpa <yvensre@gmail.com>
2018-01-27 09:38:49 -0300

    Add basic UI for game screen and add MainMessagePanel

---
+++ GGJProject/Assets/Scripts/Core/OptionButtonLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+[RequireComponent(typeof(Text))]
+public class OptionButtonLogic : MonoBehaviour
+{
+    public string OptionMessage;
+    public Text OptionText;
+
+    void Start()
+    {
+        if (OptionText == null)
+        {
+            OptionText = GetComponent<Text>();
+        }
+    }
+
+    void Update()
+    {
+    }
+}
