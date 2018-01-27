94499c4a7ad1713732a033e3465fd4e624468d57
YvensArqueiro <yvens@ar.quei.ro>
2018-01-26 22:30:57 -0300

    Add basic gameScreen and coreLogic classes

---
+++ GGJProject/Assets/Scripts/Core/GameGraph.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+public enum GameOptionResult
+{
+    POSITIVE, NEUTER, NEGATIVE
+}
+
+public class GamePhraseOption
+{
+    public string optionPhrase;
+    public GameOptionResult optionResult;
+
+    public GamePhraseOption(string optionPhrase, GameOptionResult optionResult)
+    {
+        this.optionPhrase = optionPhrase;
+        this.optionResult = optionResult;
+    }
+}
+
+public class GameNode
+{
+    public string phrase;
+    public List<GamePhraseOption> options;
+
+    public GameNode(string phrase, List<GamePhraseOption> options)
+    {
+        this.phrase = phrase;
+        this.options.AddRange(options);
+    }
+}
+
+public class GameGraph
+{
+
+}
